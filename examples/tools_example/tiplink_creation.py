8c370a2e978730914d659b01fd6689f42c86f386
Alhibb <63309522+Alhibb@users.noreply.github.com>
2025-07-04 19:55:23 +0100

    Update tiplink_creation.py

--- examples/tools_example/tiplink_creation.py
+++ examples/tools_example/tiplink_creation.py
@@ -6,10 +6,10 @@ logging.basicConfig(level=logging.INFO)
 
 def test_create_tiplink():
     agent = SolanaAgentKit(
-        private_key="23nna8ixFP4s4aJAMw5bUAh84Nkw7EyByc3YYiSokcDFtqgAz35xt34TMiFAGPzRoEe6suC9qYedpzfAo3VGBRWb",
+        private_key="",
         rpc_url="https://api.devnet.solana.com",
-        openai_api_key="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",
-        solutiofi_api_key="479d31e1-1ec2-4d73-b9b6-90324bba39cd"
+        openai_api_key="sk-proj-----",
+        solutiofi_api_key=""
     )
 
     print("Creating Tiplink...")
